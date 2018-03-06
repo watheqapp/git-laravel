b4b95feac2b5f6a72881d8a623e4b244260dcec8
watheqapp <0125811Ww>
2018-03-06 19:18:31 +0200

    Backend | Mark not login lawyer row in lawyer list

---
+++ resources/views/backend/lawyer/list.blade.php
@@ -0,0 +1,14 @@
+@extends('backend.layouts.list')
+@section('listTable')
+
+<br />
+<div class="">
+    <span class="badge badge-roundless" style="background-color: #F1C40F;">{{__('backend.clerk')}}</span>
+    <span class="badge badge-roundless" style="background-color: #36c6d3;">{{__('backend.authorized')}}</span>
+    <span class="badge badge-roundless" style="background-color: red;">{{__('backend.activeted')}}</span>
+    <span class="badge badge-roundless" style="background-color: #ccc;">{{__('backend.Not login yet')}}</span>
+    <br /><br />
+    @parent
+</div>
+
+@endsection
