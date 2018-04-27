dc1cd2abed868b74d85d349ab31b5c8f205f3e5c
watheqapp <0125811Ww>
2018-04-27 16:21:01 +0200

    Backend | Notifications | List new, pending, closed and removed orders notifications

--- resources/views/email/admin-notification.blade.php
+++ resources/views/email/admin-notification.blade.php
@@ -4,17 +4,22 @@
         <table border="0" cellpadding="0" cellspacing="0">
             <tr>
                 <td>
-                    <img src="{{asset('backend-assets//layouts/layout4/img/playstore-icon.png')}}" style="display: block; margin: 0 auto"/>
+                    <img width="200px" src="{{asset('backend-assets//layouts/layout4/img/playstore-icon.png')}}"
+                         style="display: block; margin: 0 auto"/>
                     <hr>
                 </td>
             </tr>
-            <tr>
+            <tr style="text-align: center;">
                 <td>
-                    <p>مرحبا {!! $username !!}!</p>
+                    <p>
+                        <strong>
+                            مرحبا {!! $username !!}!
+                        </strong>
+                    </p>
                     <p>{{ $body }}</p>
                     <p>
 
-                    <a href="{{route('backend.order.show', ['id' => $order->id])}}">رابط الطلب</a>
+                        <a href="{{route('backend.order.show', ['id' => $order->id])}}">رابط الطلب</a>
                     </p>
                 </td>
             </tr>
